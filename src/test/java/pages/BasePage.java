6299aa5b0e49022021f5469691714103e55a4cd8
eric ndungutse <eric.ndungutse@testsolutions.de>
2025-10-30 10:23:39 +0200

    setup multi-environment support

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -3,6 +3,7 @@ package pages;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import utils.ConfigLoader;
 
 import java.time.Duration;
 
@@ -15,12 +16,9 @@ public class BasePage {
     wait = new WebDriverWait(driver, Duration.ofSeconds(15));
     PageFactory.initElements(driver, this);
 
-
   }
 
-  public void load(String url){
-    driver.get(url);
-
+  public void load(String url) {
+    driver.get(ConfigLoader.getInstance().getBaseUrl() + url);
   }
-
 }
