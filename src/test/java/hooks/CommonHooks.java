828b464319f4c68b3c5c05b134b15fab53d88817
karangwasan <sandra.karangwa@testsolutions.de>
2025-10-30 09:55:18 +0200

    structure setup

--- src/test/java/hooks/CommonHooks.java
+++ src/test/java/hooks/CommonHooks.java
@@ -1,36 +1,26 @@
 package hooks;
 
-import com.utils.DriverProvider;
-import com.utils.RunContext;
-
+import factory.DriverFactory;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 
 import org.openqa.selenium.WebDriver;
 
-import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
 public class CommonHooks {
 
+
+  private WebDriver driver;
+
   @Before
-  public void setup() {
-    DriverProvider.get();
+  public void before() {
+    driver = DriverFactory.initializeDriver();
+
   }
 
   @After
-  public void teardown() {
-    RunContext.clear();
-    WebDriver webDriver = DriverProvider.get();
-    if (webDriver != null) {
-      try {
-        webDriver.manage().deleteAllCookies();
-        //webDriver.quit();
-      } catch (Exception e) {
-        log.error("Error during driver cleanup: {}", e.getMessage());
-      } finally {
-        DriverProvider.remove();
-      }
-    }
+  public  void  after(){
+    driver.quit();
   }
+
 }
--- src/test/java/runners/RunRegressionTests.java
+++ src/test/java/runners/RunRegressionTests.java
@@ -7,7 +7,7 @@ import static io.cucumber.junit.platform.engine.Constants.*;
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features")
-@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "steps")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "steps, hooks")
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty, html:target/cucumber-reports.html")
 @ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "@regression")
 public class RunRegressionTests {}
