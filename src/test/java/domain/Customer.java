3148afd317c6692087912d76871aaa92af0e88b4
erictestsolutions <eric.ndungutse@testsolutions.de>
2025-10-31 14:54:53 +0200

    Authendication (#3)
    * test: add feaures for login and register. create test steps for login
    
    * test: add customer registration test steps
    
    * test: add domain object in registration steps

---
+++ src/test/java/domain/Customer.java
@@ -0,0 +1,41 @@
+package domain;
+
+public class Customer {
+    private String username;
+    private String email;
+    private String password;
+
+    public Customer(String username) {
+        this(username, "default", "default");
+    }
+
+    public Customer(String username, String email, String password) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
--- src/test/java/steps/cart/AddToCartSteps.java
+++ src/test/java/steps/cart/AddToCartSteps.java
@@ -1,15 +1,15 @@
 package steps.cart;
 
+import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+
 import factory.DriverFactory;
-import io.cucumber.java.PendingException;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import org.openqa.selenium.WebDriver;
 import pages.CartPage;
 import pages.HomePage;
-import static org.junit.jupiter.api.Assertions.*;
 
 public class AddToCartSteps {
   private WebDriver driver;
@@ -29,39 +29,38 @@ public class AddToCartSteps {
 
   @Then("a View cart link appears for {string}")
   public void aLinkAppearsFor(String arg1) {
-    assertTrue(homePage.isViewCartLinkVisible(), "View cart link should be visible");
+    Assert.assertTrue("View cart link should be visible", homePage.isViewCartLinkVisible());
   }
 
   @And("cart counter increments by {int}")
   public void cartCounterIncrementsBy(int expectedCount) {
     int actualCount = homePage.getCartCount();
-    assertEquals(expectedCount, actualCount, "Cart counter value should match the expected count");
+    Assert.assertEquals(expectedCount, actualCount);
   }
 
   @And("the {string} should be in the cart with {string}, {string}, and {string}")
-  public void theShouldBeInTheCartWithAnd(String productName, String expectedPrice, String expectedQty, String expectedSubtotal) {
+  public void theShouldBeInTheCartWithAnd(String productName, String expectedPrice, String expectedQty,
+      String expectedSubtotal) {
     CartPage cartPage = new CartPage(driver);
 
     homePage.clickViewCartLink();
 
-    assertEquals(productName, cartPage.getProductName(), "Product name should match");
-    assertEquals(expectedPrice, cartPage.getProductPrice(), "Product price should match");
-    assertEquals(expectedQty, cartPage.getProductQuantity(), "Product quantity should match");
-    assertEquals(expectedSubtotal, cartPage.getProductSubtotal(), "Product subtotal should match");
+    Assert.assertEquals(productName, cartPage.getProductName());
+    Assert.assertEquals(expectedPrice, cartPage.getProductPrice());
+    Assert.assertEquals(expectedQty, cartPage.getProductQuantity());
+    Assert.assertEquals(expectedSubtotal, cartPage.getProductSubtotal());
   }
 
-
-  //Unique steps for add to cart from product detail page
-
+  // Unique steps for add to cart from product detail page
 
   @Given("Customer is on the product page for {string}")
   public void customerIsOnTheProductPageFor(String arg0) {
-    //My code goes here
+    // My code goes here
   }
 
   @Then("{string} message is displayed")
   public void hasBeenAddedToYourCartMessageIsDisplayed(String arg0) {
-   //My code will go here
+    // My code will go here
   }
 
 }
